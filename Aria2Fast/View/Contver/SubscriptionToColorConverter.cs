061d92f452f31d4e6f66fc513ec56315fea05827
aiqinxuancai <aiqinxuancai@163.com>
2024-02-24 22:46:50 +0800

    移除无用代码

--- Aria2Fast/View/Contver/SubscriptionToColorConverter.cs
+++ Aria2Fast/View/Contver/SubscriptionToColorConverter.cs
@@ -13,16 +13,7 @@ namespace Aria2Fast.View.Contver
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            // 假设您在MikanAnimeRss中有一个布尔属性IsSubscribed来代表是否已订阅
             var isSubscribed = (bool)value;
-            //if (new Random((int)DateTime.Now.Ticks).Next(1, 2) == 1)
-            //{
-            //    isSubscribed = true;
-            //} 
-            //else
-            //{
-            //    isSubscribed = false;
-            //}
             var green = (SolidColorBrush)new BrushConverter().ConvertFromString("#19be6b");
             var gray = (SolidColorBrush)new BrushConverter().ConvertFromString("#88808695");
 
