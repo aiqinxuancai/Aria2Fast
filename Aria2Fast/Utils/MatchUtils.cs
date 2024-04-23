8ab423fe26606c465282f1d876d85bfc0780aa57
aiqinxuancai <aiqinxuancai@163.com>
2024-04-23 11:22:34 +0800

    优化集数匹配

--- Aria2Fast/Utils/MatchUtils.cs
+++ Aria2Fast/Utils/MatchUtils.cs
@@ -9,8 +9,8 @@ namespace Aria2Fast.Utils
 {
     class MatchUtils
     {
-        private static readonly Regex EpisodeRegex = new Regex(@"\[\d{1,3}\]|-\s*\d{1,3}|第\d{1,4}集|第\d{1,4}话", RegexOptions.Compiled);
-        private static readonly Regex CleanUpRegex = new Regex(@"[\[\]\-\s第集话]", RegexOptions.Compiled);
+        private static readonly Regex EpisodeRegex = new Regex(@"\[\d{1,3}\]|-\s*\d{1,3}|第\d{1,4}集|第\d{1,4}话|【\d{1,3}】", RegexOptions.Compiled);
+        private static readonly Regex CleanUpRegex = new Regex(@"[\[\]\-\s第集话【】]", RegexOptions.Compiled);
 
         public static string ExtractEpisodeNumber(string title)
         {
