4fae5fd7f45b8583571c526bb961c9ab7fcc95a0
aiqinxuancai <aiqinxuancai@163.com>
2024-08-12 11:08:54 +0800

    订阅列表支持多选后右键操作

--- Aria2Fast/View/WkySubscriptionListView.xaml.cs
+++ Aria2Fast/View/WkySubscriptionListView.xaml.cs
@@ -19,6 +19,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+using Newtonsoft.Json;
 
 
 
@@ -57,8 +58,7 @@ namespace Aria2Fast.View
                 });
         }
 
-
-        private SubscriptionModel _lastSubscriptionModel;
+        List<SubscriptionModel> _selectedItems;
 
         public static readonly DependencyProperty ViewModelProperty =
             DependencyProperty.Register("ViewModel", typeof(ObservableCollection<SubscriptionModel>), typeof(WkySubscriptionListView));
@@ -89,60 +89,70 @@ namespace Aria2Fast.View
             }
         }
 
-
-
-        private void MainDataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        private void MainDataGrid_ContextMenuOpening(object sender, ContextMenuEventArgs e)
         {
-            ContextMenu menu = new ContextMenu();
+            //展开时，需要暂停刷新？
+            var contextMenu = MainDataGrid.ContextMenu;
+            contextMenu.Items.Clear();
 
-            e.Row.MouseRightButtonDown += (s, a) => {
-                a.Handled = true;
-
-                SubscriptionModel model = (SubscriptionModel)((DataGridRow)s).DataContext;
-                _lastSubscriptionModel = model;
-
-                menu.Items.Clear();
-                if (_lastSubscriptionModel != null)
+            var selectedItems = new List<SubscriptionModel>();
+            foreach (var item in MainDataGrid.SelectedItems)
+            {
+                var myItem = item as SubscriptionModel;
+                if (myItem != null)
                 {
-                    if (AppConfig.Instance.ConfigData.Aria2UseLocal)
-                    {
-                        MenuItem menuOpenPath = new MenuItem() { Header = "打开目录" };
-                        menuOpenPath.Click += MenuOpenPath_Click;
-                        menu.Items.Add(menuOpenPath);
-                    }
+                    selectedItems.Add(myItem);
+                }
+            }
 
+            var jsonString = JsonConvert.SerializeObject(selectedItems);
 
-                    MenuItem menuEdit = new MenuItem() { Header = "编辑" };
-                    menuEdit.Click += MenuEdit_Click;
+            //需要深拷贝
+            _selectedItems = JsonConvert.DeserializeObject<List<SubscriptionModel>>(jsonString);
 
-                    MenuItem menuCopyUrl = new MenuItem() { Header = "复制URL" };
-                    menuCopyUrl.Click += MenuCopyUrl_Click; ;
-
-                    MenuItem menuReDownload = new MenuItem() { Header = "重新下载" };
-                    menuReDownload.Click += MenuReDownload_Click; ;
-
-                    MenuItem menuDelete = new MenuItem() { Header = "删除" };
-                    menuDelete.Click += MenuDelete_Click;
-
-
-                    menu.Items.Add(menuEdit);
-                    menu.Items.Add(menuCopyUrl);
-                    menu.Items.Add(menuReDownload);
-                    menu.Items.Add(menuDelete);
-                    DataGrid row = sender as DataGrid;
-
-
-                    row.ContextMenu = menu;
+            if (_selectedItems.Count > 0)
+            {
+                if (AppConfig.Instance.ConfigData.Aria2UseLocal && _selectedItems.Count == 1)
+                {
+                    MenuItem menuOpenPath = new MenuItem() { Header = "打开目录" };
+                    menuOpenPath.Click += MenuOpenPath_Click;
+                    contextMenu.Items.Add(menuOpenPath);
                 }
 
-                
+                if (_selectedItems.Count == 1)
+                {
+                    MenuItem menuEdit = new MenuItem() { Header = "编辑" };
+                    menuEdit.Click += MenuEdit_Click;
+                    contextMenu.Items.Add(menuEdit);
+                }
 
-            };
+
+                MenuItem menuCopyUrl = new MenuItem() { Header = "复制URL" };
+                menuCopyUrl.Click += MenuCopyUrl_Click; ;
+
+                MenuItem menuReDownload = new MenuItem() { Header = "重新下载" };
+                menuReDownload.Click += MenuReDownload_Click; ;
+
+                MenuItem menuDelete = new MenuItem() { Header = "删除" };
+                menuDelete.Click += MenuDelete_Click;
+
+                contextMenu.Items.Add(menuCopyUrl);
+                contextMenu.Items.Add(menuReDownload);
+                contextMenu.Items.Add(menuDelete);
+                DataGrid row = sender as DataGrid;
+                row.ContextMenu = contextMenu;
+
+            }
+
+            
+
+
         }
+
 
         private void MenuEdit_Click(object sender, RoutedEventArgs e)
         {
-            var model = _lastSubscriptionModel;
+            var model = _selectedItems.FirstOrDefault();
 
             //编辑
             MainWindow.Instance.RootNavigation.Navigate(typeof(AddSubscriptionView), (model.Url, model.Name, new MikanAnime(), model));
@@ -151,7 +161,7 @@ namespace Aria2Fast.View
 
         private void MenuOpenPath_Click(object sender, RoutedEventArgs e)
         {
-            var model = _lastSubscriptionModel;
+            var model = _selectedItems.FirstOrDefault();
 
             if (Directory.Exists(model.SavePath))
             {
@@ -159,43 +169,50 @@ namespace Aria2Fast.View
                 Process.Start("explorer.exe", correctedPath);
             } 
         }
-
+        
         private void MenuCopyUrl_Click(object sender, RoutedEventArgs e)
         {
-            Clipboard.SetDataObject(_lastSubscriptionModel.Url);
-            
+            var urls = _selectedItems.Select(item => item.Url);
+            Clipboard.SetDataObject(string.Join("\r\n", urls));
         }
 
-        
         private void MenuReDownload_Click(object sender, RoutedEventArgs e)
         {
-            //SubscriptionManager.Instance.SubscriptionModel.Remove(_lastSubscriptionModel);
-            //SubscriptionManager.Instance.Save();
-
             if (!SubscriptionManager.Instance.Subscribing)
             {
-                _lastSubscriptionModel.AlreadyAddedDownloadModel = new ObservableCollection<SubscriptionSubTaskModel> { };
-                SubscriptionManager.Instance.CheckSubscriptionOne(_lastSubscriptionModel, Aria2ApiManager.Instance.CurrentRpc);
+                Task.Run(async () =>
+                {
+                    foreach (var item in _selectedItems)
+                    {
+                        item.AlreadyAddedDownloadModel = new ObservableCollection<SubscriptionSubTaskModel> { };
+                        await SubscriptionManager.Instance.CheckSubscriptionOne(item, Aria2ApiManager.Instance.CurrentRpc);
+                    }
+                });
             }
             else
             {
-                MainWindow.Instance.ShowSnackbar("当前无法操作", $"正在执行订阅中...");
+                MainWindow.Instance.ShowSnackbar("无法操作", $"正在执行订阅中");
             }
 
         }
 
         private void MenuDelete_Click(object sender, RoutedEventArgs e)
         {
-            SubscriptionManager.Instance.SubscriptionModel.Remove(_lastSubscriptionModel);
+            foreach (var item in _selectedItems)
+            {
+                SubscriptionManager.Instance.SubscriptionModel.Remove(item);
+            }
             SubscriptionManager.Instance.Save();
         }
 
         private void SubscriptionButton_Click(object sender, RoutedEventArgs e)
         {
-            //WindowAddSubscription.Show(Application.Current.MainWindow);
-
-
             MainWindow.Instance.RootNavigation.Navigate(typeof(AddSubscriptionView), null);
+
+        }
+
+        private void MainDataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
 
         }
     }
